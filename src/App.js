fd0da67690a9e50b0a693eea9e4d1a5d095f04b3
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-20 22:54:19 +0000

    Add NavBar and initial styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
