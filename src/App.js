a9a180d63c49b90956b1aef8bdbc640d89991c22
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-20 22:30:09 +0000

    Add Bootstrap script and test using primary button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
