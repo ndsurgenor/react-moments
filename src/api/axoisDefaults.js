2d021683b02da375962c79041f24244a25ade9f1
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-22 21:03:45 +0000

    Add axios interceptors

--- src/api/axoisDefaults.js
+++ src/api/axoisDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://nds-drf-api-b69fd84855e5.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
