1e0fe203d712af573a8d91dd1e4f37ff2adaccb6
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-22 22:14:14 +0000

    Add code for cleaner UX with burger icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,16 +6,18 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
-    const handleSignOut = async() => {
+    const handleSignOut = async () => {
         try {
             await axios.post('dj-rest-auth/logout/');
             setCurrentUser(null);
-        } catch(err) {
+        } catch (err) {
             console.log(err);
         }
     };
@@ -83,7 +85,11 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar
+            expanded={expanded}
+            className={styles.NavBar}
+            expand="md"
+            fixed="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
@@ -91,7 +97,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle
+                    onClick={() => setExpanded(!expanded)}
+                    ref={ref}
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink
