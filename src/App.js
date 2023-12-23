bbf5d617dcc277d1877fad76d47ac1162ff9c6e1
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-23 01:30:18 +0000

    Add ability to upload photo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import './api/axoisDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 function App() {
-
   return (
     <div className={styles.App}>
       <NavBar />
@@ -17,6 +15,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
