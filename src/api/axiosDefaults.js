3817f6d195eeb79b9ec4989a6fd5815f846e78ee
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-27 22:03:14 +0000

    Add code to submit post

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://nds-drf-api-b69fd84855e5.herokuapp.com/"
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
