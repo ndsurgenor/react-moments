730522d1d5ef9cd1b3da579e513c0945dccf7844
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-30 22:55:25 +0000

    Write remaining tests for NavBar

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,20 +1,23 @@
-import rest from 'msw';
+import { rest } from "msw";
 
-const baseURL = "https://nds-drf-api-b69fd84855e5.herokuapp.com/"
+const baseURL = "https://drf-api-rec.herokuapp.com/";
 
 export const handlers = [
-    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
-        return res(ctx.json({
-            pk: 2,
-            username: "ndsurgenor",
-            email: "",
-            first_name: "",
-            last_name: "",
-            profile_id: 2,
-            profile_image: "https://res.cloudinary.com/dzgxvsnpr/image/upload/v1/media/../default_profile_n85cdj.jpg",
-        }))
-    }),
-    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
-        return res(ctx.status(200))
-    }),
-] 
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "brian",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/dzgxvsnpr/image/upload/v1/media/../default_profile_n85cdj.jpg",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
